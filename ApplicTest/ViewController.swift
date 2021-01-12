a1e1e5441adba26d732f52fef319d6b0b729aee9
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-12 23:00:42 +0300

    main without wnyth

--- ApplicTest/ViewController.swift
+++ ApplicTest/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
+//main without anything
 
 }
 
